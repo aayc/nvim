e950013aaca0b03b0da703eb225d9e078c5e4915
Aaron Chan <aaronchan@microsoft.com>
2024-02-05 09:32:29 -0500

    Add lsp config

--- init.lua
+++ init.lua
@@ -7,6 +7,10 @@ vim.o.smartcase = true
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
+-- Disable line wrap
+vim.wo.wrap = false
+
+-- List of 
 
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -30,7 +34,7 @@ require("lazy").setup({
     {
         -- File search
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
-         dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = { 'nvim-lua/plenary.nvim' },
     }, {
         -- Lang parsing
         "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
@@ -46,7 +50,7 @@ require("lazy").setup({
     },
     {
         -- Color theme
-        "olimorris/onedarkpro.nvim",
+        'martinsione/darkplus.nvim',
         priority = 1000, -- Ensure it loads first
     },
     {
@@ -67,8 +71,6 @@ require("lazy").setup({
     {
         -- Mason for LSP, formatters, and more
         "williamboman/mason.nvim",
-        opts = {},
-        lazy = false,
         dependencies = {
             "williamboman/mason-lspconfig.nvim",
         },
@@ -91,8 +93,8 @@ require("lazy").setup({
                     "bashls",
                     "cssls",
                     "dockerls",
-                    "gopls",
                     "html",
+                    "tailwindcss",
                     "jsonls",
                     "pyright",
                     "tsserver",
@@ -105,6 +107,81 @@ require("lazy").setup({
     },
     {
         "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            { "antosha417/nvim-lsp-file-operations", config = true },
+        },
+        config = function()
+            local lspconfig = require("lspconfig")
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+            local keymap = vim.keymap
+            local opts = { noremap = true, silent = true }
+            local on_attach = function(client, bufnr)
+                opts.buffer = bufnr
+                opts.desc = "Show LSP references"
+                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+
+                opts.desc = "Go to declaration"
+                keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                
+                opts.desc = "Show LSP definitions"
+                keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+
+                opts.desc = "Show LSP implementations"
+                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+                
+                opts.desc = "Show LSP type definitions"
+                keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+                opts.desc = "See available code actions"
+                keymap.set("n", "gt", vim.lsp.buf.code_action, opts)
+
+                opts.desc = "Smart rename"
+                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+                opts.desc = "Show buffer diagnostics"
+                keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+                
+                opts.desc = "Show line diagnostics"
+                keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+                opts.desc = "Go to previous diagnostic"
+                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+                opts.desc = "Go to next diagnostic"
+                keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+                opts.desc = "Show documentation for what is under cursor"
+                keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+                opts.desc = "Restart LSP"
+                keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+            end
+
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+
+            for lang in { "html", "tsserver", "pyright", "tailwindcss" } do
+                lspconfig[lang].setup({
+                    capabilities = capabilities,
+                    on_attach = on_attach,
+                })
+            end
+
+            lspconfig["lua_ls"].setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                }
+            })
+
+
+        end,
     },
     {
         -- Commenting
@@ -121,18 +198,72 @@ require("lazy").setup({
         'folke/which-key.nvim',
         opts = {}
     },
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+            cmp.setup({
+                completion = {
+                    completeopt = "menu,menuone,preview,noselect"
+                },
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-k>"] = cmp.mapping.select_prev_item(),
+                    ["<C-j>"] = cmp.mapping.select_next_item(),
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" }
+                })
+            })
+        end
+    }
+    -- https://github.com/dccsillag/magma-nvim for jupyter
+    -- vim fugitive
 }, { lazy = true })
     
 
-
+-- Configure telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local telescope_actions = require('telescope.actions')
+require("telescope").setup({
+    defaults = {
+	mappings = {
+	    i = {
+		["<esc>"] = telescope_actions.close,
+
+	    }
+	}
+    }
+})
 
 -- Configure theme
-vim.cmd("colorscheme onedark")
+vim.cmd("colorscheme darkplus")
 
 -- configure oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
